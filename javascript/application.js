e5ba7b7b06b9b184b945d3b460236bb4dd378d0d
vincent-kubis <vincent.kubis@gmail.com>
2015-10-16 18:07:17 +0200

    tst

--- javascript/application.js
+++ javascript/application.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-var controller = new ScrollMagic.Controller();
+//var controller = new ScrollMagic.Controller();
 	$("#profil").fadeTo(3000, 1);
 	  if (window.innerWidth<768) {
 	    new Vivus('mobile', {
@@ -8,24 +8,24 @@ var controller = new ScrollMagic.Controller();
 			type: 'oneByOne',
 			duration: 150
 			});
-	    new ScrollMagic.Scene({
-        	duration: 700,  // the scene should last for a scroll distance of 100px
-        	offset: 0      // start this scene after scrolling for 50px
-    })
-	  	    .setPin("#home") // pins the element for the the scene's duration
-    		.addTo(controller); // assign the scene to the controller
+	    //new ScrollMagic.Scene({
+        	//duration: 700,  // the scene should last for a scroll distance of 100px
+        	//offset: 0      // start this scene after scrolling for 50px
+    //})
+	  	    //.setPin("#home") // pins the element for the the scene's duration
+    		//.addTo(controller); // assign the scene to the controller
 	  }else if (window.innerWidth>970 ) {
 	  	new Vivus('computer', {
 		    start: 'inViewport',
 		    type: 'oneByOne',
 		    duration: 150
 			});
-	  	new ScrollMagic.Scene({
-        	duration: 750,  // the scene should last for a scroll distance of 100px
-        	offset: 0      // start this scene after scrolling for 50px
+	  	//new ScrollMagic.Scene({
+        	//duration: 750,  // the scene should last for a scroll distance of 100px
+        	//offset: 0      // start this scene after scrolling for 50px
     })
-	  	    .setPin("#home") // pins the element for the the scene's duration
-    		.addTo(controller); // assign the scene to the controller
+	  	    //.setPin("#home") // pins the element for the the scene's duration
+    		//.addTo(controller); // assign the scene to the controller
 
 	  }else{
 	  	new Vivus('tablet', {
@@ -33,12 +33,12 @@ var controller = new ScrollMagic.Controller();
 		    type: 'oneByOne',
 		    duration: 150
 			});
-	  	new ScrollMagic.Scene({
-        	duration: 1100,  // the scene should last for a scroll distance of 100px
-        	offset: 0      // start this scene after scrolling for 50px
-    	})
-	  	    .setPin("#home") // pins the element for the the scene's duration
-    		.addTo(controller); // assign the scene to the controller
+	  	//new ScrollMagic.Scene({
+        	//duration: 1100,  // the scene should last for a scroll distance of 100px
+        	//offset: 0      // start this scene after scrolling for 50px
+    	//})
+	  	    //.setPin("#home") // pins the element for the the scene's duration
+    		//.addTo(controller); // assign the scene to the controller
 	  };
 
 	$("#demo01").animatedModal({
